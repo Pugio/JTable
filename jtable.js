9287480ac998fc04926504e38db1e3b65d7cf9af
Shalev <pugiox@gmail.com>
2016-02-23 00:49:51 -0500

    Install foundation, sass, and setup basic styles.

--- jtable.js
+++ jtable.js
@@ -14,6 +14,10 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+
+  Template.hello.onRendered(function () {
+    $(document).foundation();
+  });
 }
 
 if (Meteor.isServer) {
