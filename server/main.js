1c91fb64c2d225188dc65bf59e04c7b4e55696d0
Daniel Newman <dnewman@kyruus.com>
2016-03-29 21:09:23 -0400

    added a collection for testing to be deleted later

--- server/main.js
+++ server/main.js
@@ -1,3 +1,5 @@
 Meteor.startup(function () {
   // code to run on server at startup
+  DeleteThisCollection = new Mongo.Collection("deletecollection");
+  DeleteThisCollection.insert({ text: "Hello world!", createdAt: new Date() });
 });
