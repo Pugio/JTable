7ee0b19714c823b83b8f6fa258de5fcdc4ee609f
Shalev <pugiox@gmail.com>
2016-04-12 22:09:26 -0400

    Initial meal form submission with login.

--- client/templates/meal_form/meal_form.js
+++ client/templates/meal_form/meal_form.js
@@ -7,19 +7,71 @@ Template.meal_form.rendered = function () {
   $('html, body').animate({scrollTop: $("#meal-form").offset().top}, animation_props);
 }
 
-Template.meal_form.helpers({
-  guest: function() {
-    return Session.get('showMealForm') == 'find';
+function submit_meal_form() {
+  // Meteor.
+  alert('submitting!!');
+  // Insert a task into the collection
+  Tasks.insert({
+    text,
+    createdAt: new Date(), // current time
+  });
+  
+  // Clear form
+  target.text.value = '';
+}
+
+Template.meal_form.events({
+
+  // Triggered upon form submission if the user is NOT logged in
+  'click .login-submit-meal': function (event) {
+    event.preventDefault();
+
+    if (Meteor.userId())  // we actually are logged in
+      return submit_meal_form();
+
+    Meteor.loginWithFacebook(function (err) {
+      if (err)
+        ;//bad
+      else
+        submit_meal_form();
+    });
   },
+
+  // Triggered upon form submission if the user IS logged in
+  'click .submit-meal': function (event) {
+     // Prevent default browser form submit
+     event.preventDefault();
+
+     submit_meal_form();
+   }
+});
+
+
+
+function isGuest() { return Session.get('showMealForm') == 'find'; }
+
+Template.meal_form.helpers({
+  guest: isGuest,
 
   mealFormType: function() {
     var meal_form_type = Session.get('showMealForm');
     return meal_form_type.charAt(0).toUpperCase() + meal_form_type.slice(1);
+  },
+
+  meat_dairy_type: function() {
+    return isGuest() ? 'checkbox' : 'radio';
   }
 });
 
 Template.meal_form_datepicker.rendered = function () {
+  var today = new Date(), picker_start,
+      days_from_now = 5 - today.getDay(),
+      picker_start = days_from_now > 0 ?
+                      new Date(today.valueOf() + 86400000 * days_from_now) :
+                      today;
+  
+  abc = picker_start;
   this.$('input').fdatepicker({
-    daysOfWeekDisabled: [0,1,2,3,4] // TODO: eventually update this to include yomim tovimte
+    daysOfWeekDisabled: [0,1,2,3,4] // TODO: eventually update this to include yomim tovim
   });
 }
